ba38317ea60ddc578184397cbf76df01c60b4394
Babar Raza <babar.raza@aspose.com>
2024-03-06 19:45:26 +0500

    Update dispatch-all.js

--- .github/scripts/dispatch-all.js
+++ .github/scripts/dispatch-all.js
@@ -1,19 +1,26 @@
 const fs = require('fs');
 
-const workflowsDirectory = '../workflows'; 
-const workflows = fs.readdirSync(workflowsDirectory).filter(file => file.endsWith('.yml'));
+async function dispatchAllWorkflows() {
+  const workflowsDirectory = '../workflows'; // Change to the directory where your workflows are stored
+  const workflows = fs.readdirSync(workflowsDirectory).filter(file => file.endsWith('.yml'));
 
-for (const workflow of workflows) {
-  const workflowName = workflow.replace('.yml', '');
-  const response = await octokit.request('POST /repos/Aspose/products.aspose.app-workflows/actions/workflows/{workflow}/dispatches', {
-    owner: 'Aspose',
-    repo: 'products.aspose.app-workflows',
-    workflow: 'workflowName',
-    ref: 'main', 
-    inputs: {
-      environment: process.env.ENVIRONMENT
-    }
-  });
+  for (const workflow of workflows) {
+    const workflowName = workflow.replace('.yml', '');
+    const response = await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow}/dispatches', {
+      owner: process.env.GITHUB_REPOSITORY.split("/")[0],
+      repo: process.env.GITHUB_REPOSITORY.split("/")[1],
+      workflow: workflowName,
+      ref: 'main', // Replace with your branch name
+      inputs: {
+        environment: process.env.ENVIRONMENT
+      }
+    });
 
-  console.log(`Dispatched ${workflowName} workflow for environment: ${process.env.ENVIRONMENT} - Status: ${response.status}`);
+    console.log(`Dispatched ${workflowName} workflow for environment: ${process.env.ENVIRONMENT} - Status: ${response.status}`);
+  }
 }
+
+dispatchAllWorkflows().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
